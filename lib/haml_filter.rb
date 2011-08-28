1153cacfe1110b3bba3d69b9a73679a23c00f96c
Jordon Bedwell <jordon@envygeeks.com>
2011-08-28 13:17:25 -0500

    Skip Haml and send original text on a Haml error. Fixes: #2 Signed-off-by: Jordon Bedwell <jordon@envygeeks.com>

--- lib/haml_filter.rb
+++ lib/haml_filter.rb
@@ -1,6 +1,12 @@
 class HamlFilter < TextFilter
   #description_file File.dirname(__FILE__) + "/../textile.html"
   def filter(text)
-    Haml::Engine.new(text).render
+    original_text = text
+    
+    begin
+      Haml::Engine.new(text).render
+    rescue Haml::SyntaxError
+      original_text # Stop Nesting ;)
+    end
   end
 end
